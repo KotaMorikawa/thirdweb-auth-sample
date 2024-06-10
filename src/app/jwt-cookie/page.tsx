eb21bb92c2305df398105081fb6648baab92bd3c
KotaMorikawa <koke.kokota0130@gmail.com>
2024-06-10 15:12:27 +0900

    chain設定を削除

--- src/app/jwt-cookie/page.tsx
+++ src/app/jwt-cookie/page.tsx
@@ -8,7 +8,7 @@ const JWTCookiePage: NextPage = () => {
   return (
     <div>
       <h2>Step 1: Connect Wallet</h2>
-      <ConnectButton client={client} wallets={wallets} chain={chain} />
+      <ConnectButton client={client} wallets={wallets} />
       <h2>Step 2: Sign the login payload</h2>
       <LoginButton />
     </div>
