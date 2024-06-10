66dd34ada086b533fb55924965f319a0c7fafc68
KotaMorikawa <koke.kokota0130@gmail.com>
2024-06-10 14:35:48 +0900

    network追加

--- src/app/jwt-cookie/page.tsx
+++ src/app/jwt-cookie/page.tsx
@@ -2,13 +2,13 @@
 import type { NextPage } from "next";
 import { ConnectButton } from "thirdweb/react";
 import { LoginButton } from "./components/login-button";
-import { client } from "../../lib/client";
+import { chain, client, wallets } from "../../lib/client";
 
 const JWTCookiePage: NextPage = () => {
   return (
     <div>
       <h2>Step 1: Connect Wallet</h2>
-      <ConnectButton client={client} />
+      <ConnectButton client={client} wallets={wallets} chain={chain} />
       <h2>Step 2: Sign the login payload</h2>
       <LoginButton />
     </div>
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,5 @@
-import { createThirdwebClient } from "thirdweb";
+import { createThirdwebClient, defineChain } from "thirdweb";
+import { createWallet, inAppWallet, walletConnect } from "thirdweb/wallets";
 
 const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!;
 const secretKey = process.env.THIRDWEB_SECRET_KEY;
@@ -10,3 +11,31 @@ export const client = createThirdwebClient(
         clientId,
       }
 );
+
+export const chain = defineChain({
+  id: 5039,
+  rpc: "https://subnets.avax.network/onigiri/testnet/rpc",
+  name: "ONIGIRI Testnet",
+  nativeCurrency: {
+    name: "ONIGIRI",
+    symbol: "ONIGIRI",
+    decimals: 18,
+  },
+  blockExplorers: [
+    {
+      name: "ONIGIRI Explorer",
+      url: "https://subnets-test.avax.network/onigiri",
+    },
+  ],
+  testnet: true,
+});
+
+export const wallets = [
+  walletConnect(),
+  createWallet("io.metamask"),
+  inAppWallet({
+    auth: {
+      options: ["email", "google", "apple", "facebook", "phone"],
+    },
+  }),
+];
